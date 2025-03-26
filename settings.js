4cd6ce617818b21c9cd4b7b99a603d4ce6f665f5
maxbot06 <alexinorjasmene@gmail.com>
2025-03-26 15:50:11 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Max',
+  author: '乂🤡𝑆𝛪𝑅~𝛭𝛥𝛸🤡乂',
+  botName: "Max Bot",
+  botOwner: '乂🤡𝑆𝛪𝑅~𝛭𝛥𝛸🤡乂', // Your name
+  ownerNumber: '50931284315', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
